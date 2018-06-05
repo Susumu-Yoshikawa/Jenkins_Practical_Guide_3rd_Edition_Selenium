60e6b7845863917945fedd4847c597867d769fdc
yoshikawasusumu <yoshikawasusumu@192.168.23.156>
2018-06-05 13:18:44 +0900

    aa

--- src/test/java/jp/gihyo/jenkinsbook/webdriver/SampleTestCase.java
+++ src/test/java/jp/gihyo/jenkinsbook/webdriver/SampleTestCase.java
@@ -21,22 +21,22 @@ public class SampleTestCase {
 
 	private static Properties prop = new Properties();
 	private static WebDriver driver;
-	
+
 	@BeforeClass
 	public static void setUpClass() throws IOException {
-		prop.load(new FileInputStream("target\\test-classes\\selenium.properties"));
+		prop.load(new FileInputStream("target/test-classes/selenium.properties"));
 		driver = new FirefoxDriver();
 	}
-	
+
 	@AfterClass
 	public static void tearDownClass() throws IOException {
 		driver.quit();
 	}
-	
+
 	@Test
 	public void testNormal01() {
 		driver.get(prop.getProperty("baseUrl") + "/sampleproject");
-		
+
 		TopPage topPage = new TopPage(driver);
 		assertEquals("名字", topPage.getLastNameLabel());
 		assertEquals("名前", topPage.getFirstNameLabel());
@@ -45,16 +45,16 @@ public class SampleTestCase {
 		assertTrue(topPage.hasFirstNameInput());
 		assertTrue(topPage.hasSubmit());
 	}
-	
+
 	@Test
 	public void testNormal02() {
 		driver.get(prop.getProperty("baseUrl") + "/sampleproject");
-		
+
 		TopPage topPage = new TopPage(driver);
 		topPage.setLastName("Hoge");
 		topPage.setFirstName("Foo");
 		topPage.submit();
-		
+
 		String greeting = "";
 		Calendar calendar = Calendar.getInstance();
 		int hour = calendar.get(Calendar.HOUR_OF_DAY);
@@ -66,15 +66,15 @@ public class SampleTestCase {
 		ResultPage resultPage = new ResultPage(driver);
 		assertEquals(greeting + ", Hoge Foo!!", resultPage.getText());
 	}
-	
+
 	@Test
 	public void testError01() {
 		driver.get(prop.getProperty("baseUrl") + "/sampleproject");
-		
+
 		TopPage page = new TopPage(driver);
 		page.setFirstName("Hoge");
 		page.submit();
-		
+
 		ResultPage resultPage = new ResultPage(driver);
 		assertEquals("エラー", resultPage.getText());
 	}
@@ -82,11 +82,11 @@ public class SampleTestCase {
 	@Test
 	public void testError02() {
 		driver.get(prop.getProperty("baseUrl") + "/sampleproject");
-		
+
 		TopPage page = new TopPage(driver);
 		page.setLastName("Hoge");
 		page.submit();
-		
+
 		ResultPage resultPage = new ResultPage(driver);
 		assertEquals("エラー", resultPage.getText());
 	}
